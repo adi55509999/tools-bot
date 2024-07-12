358e59ba10e1fc89fba86d33acf7ea07332897f7
thealexsandro <jevringarmindo@gmail.com>
2024-07-12 13:10:07 +0700

    fixing bug

--- variables.js
+++ variables.js
@@ -4,7 +4,7 @@ const userList = [ 5790165296, 1434949478 ]
 const maxCon = 1000
 const maxCusFile = 100
 const defaultIndex = 1
-const version = `5.3.3`
+const version = `5.3.4`
 
 const variables = {
     token,
